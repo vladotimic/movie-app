e0e8cbfee5b7bff9bb2de39355624ce62b1b71fc
Vladislav Timić <vtimic@gmail.com>
2023-01-12 10:11:58 +0100

    added json data files

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+import popular from './popular.json';
+import horror from './horror.json';
+import thriller from './thriller.json';
+import action from './action.json';
+import comedy from './comedy.json';
+
+export { action, comedy, horror, thriller, popular };
