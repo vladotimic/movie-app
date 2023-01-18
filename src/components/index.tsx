5a05a4b5cb4e2f24b735f24cc7d30d7429834a74
Vladislav Timić <vtimic@gmail.com>
2023-01-18 08:50:40 +0100

    deleted movieinfo component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,6 @@ import Card from './Card';
 import MovieSection from './MovieSection';
 import CastCard from './CastCard';
 import CastSection from './CastSection';
-import MovieInfo from './MovieInfo';
 import Header from './Header';
 import Banner from './Banner';
 import Poster from './Poster';
@@ -11,7 +10,6 @@ import SlideButton from './SlideButton';
 export {
   Card,
   MovieSection,
-  MovieInfo,
   CastCard,
   CastSection,
   Header,
--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,10 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Container } from '@chakra-ui/react';
+import { Container, Text, Flex, Box, Tag, Button } from '@chakra-ui/react';
+import { FaPlay } from 'react-icons/fa';
 import { MovieBanner, MovieDetails } from '@/types/movie';
 import { getPopularMovies, getMovieDetails } from '@/lib/movies';
-import { Banner, MovieInfo, CastSection } from '@/components';
+import { getYear, formatDate } from '@/utils/date';
+import { Banner, CastSection, Poster } from '@/components';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const data = await getPopularMovies();
@@ -31,6 +33,30 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       ...movie,
     },
   };
+};
+
+const TextDesc = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Text
+      fontSize="lg"
+      fontWeight="700"
+      mt="0.5rem"
+    >
+      {title}:{' '}
+      <Text
+        as="span"
+        fontWeight="400"
+      >
+        {children}
+      </Text>
+    </Text>
+  );
 };
 
 export default function MoviePage(props: MovieDetails) {
@@ -60,17 +86,122 @@ export default function MoviePage(props: MovieDetails) {
         <title>{title}</title>
       </Head>
       <Banner imgUrl={backdrop_path}>
-        <MovieInfo
-          title={title}
-          director={director}
-          overview={overview}
-          releaseDate={release_date}
-          genres={genres}
-          imgUrl={poster_path}
-          tagline={tagline}
-          lang={spoken_languages[0].name}
-          status={status}
-        />
+        <Flex
+          flexDirection={{ base: 'column', md: 'row' }}
+          gap={5}
+        >
+          <Box
+            w="100%"
+            maxW="22rem"
+            alignSelf="center"
+          >
+            <Poster imgUrl={poster_path} />
+          </Box>
+          <Box
+            w={{ base: '100%', md: '70%' }}
+            mt={{ base: '3rem', md: 0 }}
+          >
+            <Text
+              fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
+              fontWeight="700"
+            >
+              {title}
+              <Text
+                as="span"
+                ml="0.5rem"
+              >
+                ({getYear(release_date)})
+              </Text>
+            </Text>
+
+            <Box
+              display="flex"
+              alignItems="center"
+              flexWrap="wrap"
+              my="0.5rem"
+            >
+              <Box
+                display="flex"
+                mt="0.5rem"
+              >
+                <Text
+                  fontSize="lg"
+                  fontWeight="500"
+                  mr="0.5rem"
+                >
+                  Status:
+                </Text>
+                <Tag
+                  mx="0.5rem"
+                  borderRadius="full"
+                  variant="solid"
+                  colorScheme={status === 'Released' ? 'green' : 'red'}
+                >
+                  {status}
+                </Tag>
+              </Box>
+
+              <Box
+                display="flex"
+                mt="0.5rem"
+              >
+                <Text
+                  fontSize="lg"
+                  fontWeight="500"
+                  mr="0.5rem"
+                >
+                  Genres:
+                </Text>
+                <Box>
+                  {genres.map((genre) => {
+                    const { id, name } = genre;
+                    return (
+                      <Tag
+                        key={id}
+                        m="0.2rem"
+                        borderRadius="full"
+                        variant="solid"
+                      >
+                        {name}
+                      </Tag>
+                    );
+                  })}
+                </Box>
+              </Box>
+            </Box>
+
+            <Text
+              mt="0.5rem"
+              fontSize="md"
+              fontStyle="italic"
+              color="gray.200"
+            >
+              {tagline}
+            </Text>
+
+            <Text
+              mt="1rem"
+              fontSize="lg"
+              fontWeight="700"
+            >
+              Overview
+            </Text>
+            <Text my="0.5rem">{overview}</Text>
+
+            <TextDesc title="Director">{director}</TextDesc>
+            <TextDesc title="Original Language">
+              {spoken_languages[0].name}
+            </TextDesc>
+            <TextDesc title="Release Date">{formatDate(release_date)}</TextDesc>
+
+            <Button
+              mt="1rem"
+              leftIcon={<FaPlay />}
+            >
+              Play trailer
+            </Button>
+          </Box>
+        </Flex>
       </Banner>
       <Container maxW="container.xl">
         <CastSection casts={cast} />
