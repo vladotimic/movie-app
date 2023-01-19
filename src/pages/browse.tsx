633e8c8f31fc2df09d3c4c5f3bd317d1c148fdbc
Vladislav Timić <vtimic@gmail.com>
2023-01-19 15:17:51 +0100

    implemented search form and movie card results ui

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import {
   FormControl,
   FormLabel,
@@ -6,6 +7,7 @@ import {
   Button,
   Container,
   Box,
+  Text,
   Spinner,
 } from '@chakra-ui/react';
 import { searchMovies } from '@/lib/movies';
@@ -13,9 +15,15 @@ import { IMovieCard } from '@/types/movie';
 import { MovieCard } from '@/components';
 
 export default function BrowsePage() {
+  const router = useRouter();
+  const query = router.query.query;
+
   const [search, setSearch] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [results, setResults] = useState<IMovieCard[]>([]);
+  const [totalResults, setTotalResults] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
+  const [numOfPages, setNumOfPages] = useState<number>(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -27,13 +35,28 @@ export default function BrowsePage() {
     }
   };
 
+  useEffect(() => {
+    if (query) {
+      setSearch(query.toString().replaceAll('%20', ' ').trim());
+    }
+  }, [query]);
+
   const handleSearch = async () => {
     setIsLoading(true);
     try {
       const res = await searchMovies(search);
       if (res) {
-        const { data } = res;
-        setResults(data);
+        console.log({ res });
+        setResults(res.data);
+        setPage(1);
+        setTotalResults(res.total_results);
+        setNumOfPages(res.total_pages);
+        router.push({
+          pathname: `/browse`,
+          query: {
+            query: encodeURI(search),
+          },
+        });
       }
     } catch (error) {
       console.error(error);
@@ -45,29 +68,67 @@ export default function BrowsePage() {
   return (
     <Container maxW="container.xl">
       <Box
+        position="relative"
         display="flex"
-        alignItems="flex-end"
-        gap="2"
-        my={5}
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        h="15rem"
       >
-        <FormControl>
-          <FormLabel fontSize="2xl">Search Movies</FormLabel>
-          <Input
-            type="text"
-            value={search}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-          />
-        </FormControl>
-        <Button onClick={handleSearch}>Search</Button>
+        <Box
+          display="flex"
+          alignItems="flex-end"
+          w="35rem"
+        >
+          <FormControl>
+            <FormLabel fontSize="2xl">Search Movies</FormLabel>
+            <Input
+              type="text"
+              value={search}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              borderColor="red"
+              borderRadius="none"
+              borderTopLeftRadius="0.3rem"
+              borderBottomLeftRadius="0.3rem"
+              _hover={{
+                borderColor: 'red',
+              }}
+              _focusVisible={{
+                borderColor: 'red',
+              }}
+            />
+          </FormControl>
+          <Button
+            onClick={handleSearch}
+            borderRadius="none"
+            borderTopRightRadius="0.3rem"
+            borderBottomRightRadius="0.3rem"
+            minW="5rem"
+          >
+            Search
+          </Button>
+        </Box>
       </Box>
+
+      {results.length > 0 && <Text>Total results: {totalResults}</Text>}
 
       <Box
         w="100%"
         minH={isLoading ? '45rem' : 'auto'}
         display="flex"
-        justifyContent={isLoading ? 'center' : 'space-between'}
-        alignItems={isLoading ? 'center' : 'flex-start'}
+        justifyContent={{
+          base: 'center',
+          md: isLoading ? 'center' : 'space-between',
+        }}
+        alignItems={{
+          base: 'center',
+          md: isLoading ? 'center' : 'flex-start',
+        }}
+        gap={{
+          base: 5,
+          md: 0,
+        }}
         flexWrap="wrap"
       >
         {isLoading && (
